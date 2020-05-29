8f2b35a22f3e6529b8bd065f80b1fb6e49b905cb
Nguyễn Trường Giang <38888083+nguyengiangk62uet@users.noreply.github.com>
2020-05-29 23:57:03 +0700

    Delete from array

--- src/DataStructure/Array.java
+++ src/DataStructure/Array.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class Array {
 	
 	public static void insertIntoArray(int[] arr, int item, int index) {
+		System.out.println("Insert into array:");
 		int j = arr.length - 1;
 		int k = 0;
 		int[] newArr = new int[arr.length + 1];
@@ -21,12 +22,20 @@ public class Array {
 		System.out.println("After array:" + Arrays.toString(newArr));
 	}
 	
-	public static void deleteItemFromArray() {
-		
+	public static void deleteItemFromArray(int[] arr, int index) {
+		System.out.println("Delete item from array:");
+		int[] newArr = new int[arr.length - 1];
+		for (int i = 0, k = 0; i < arr.length; i++) {
+			if (i == index) continue;
+			newArr[k++] = arr[i];
+		}
+		System.out.println("Original array:" + Arrays.toString(arr));
+		System.out.println("After array:" + Arrays.toString(newArr));
 	}
 	
 	public static void main(String args[]) {
 		int[] a = {1, 2, 3, 4, 5};
 		insertIntoArray(a, 10, 3);
+		deleteItemFromArray(a, 2);
 	}
 }
